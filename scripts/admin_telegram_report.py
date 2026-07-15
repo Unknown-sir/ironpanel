05881afeb0e78864e771b02cb430f1eb816412cd
Mr.Rskhi <93010742+Unknown-sir@users.noreply.github.com>
2026-07-15 06:22:46 +0330

    Delete scripts directory

--- scripts/admin_telegram_report.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from app import create_app
-from app.services.admin_bot import admin_bot_settings, send_test_admin_report
-app = create_app()
-with app.app_context():
-    s = admin_bot_settings()
-    if s.get('enabled'):
-        send_test_admin_report()
