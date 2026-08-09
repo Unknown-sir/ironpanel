049651abd83cf53fec7b47f416ffef5936d1c2e2
Mr.Rskhi <93010742+Unknown-sir@users.noreply.github.com>
2026-08-09 19:46:49 +0330

    Delete run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-from app.core.extensions import socketio
-
-app = create_app()
-
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=app.config['PANEL_PORT'])
